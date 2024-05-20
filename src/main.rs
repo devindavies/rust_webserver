e407984b4464693297f3f21e0d1f1d367ba6f599
Devin Davies <devin.m.davies@gmail.com>
2024-05-20 10:24:15 -0400

    multi-threaded

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rust_webserver::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
@@ -6,11 +7,14 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
